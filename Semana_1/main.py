e929f1e58030d6456a2c7254f77a20df0c6ddf71
valentinvazquez-web <valentinvazquez1105@gmail.com>
2025-08-29 01:33:54 -0300

    Actual Guia

--- Semana_1/main.py
+++ Semana_1/main.py
@@ -9,7 +9,7 @@ guia.saludar(nombre)
 num_1 = int(input("Numero 1: "))
 num_2 = int(input("Numero 2: "))
 
-resultado_suma, resultado_resta, resultado_multiplicacion = guia.operar(num_1, num_2)
+guia.operar(num_1, num_2)
 
 base_triangulo = float(input("Base del triangulo: "))
 altura_triangulo = float(input("Altura del triangulo: "))
@@ -21,3 +21,10 @@ num_2 = int(input("Numero 2: "))
 num_3 = int(input("Numero 3: "))
 
 guia.buscar_mayor(num_1, num_2, num_3)
+
+num_de_edad = int(input("ingrese su edad: "))
+
+guia.verificar_acceso(num_de_edad)
+
+año_nacimiento = int(input("Ingrese año de nacimiento:"))
+guia.calcular_edad(año_nacimiento)
